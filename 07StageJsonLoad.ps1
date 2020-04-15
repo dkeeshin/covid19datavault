33024b143b44c08520c985afaf48399f338eb3ec
dkeeshin <62847782+dkeeshin@users.noreply.github.com>
2020-04-14 20:42:24 -0500

    Database and powershell update

---
+++ 07StageJsonLoad.ps1
@@ -0,0 +1,26 @@
+﻿#07StageJsonLoad load json from Web request
+#20200403 DLK
+CLS
+
+$request = 'https://covidtracking.com/api/states/daily' 
+$fromWeb = Invoke-WebRequest $request 
+$destinationServerName = "DatabaseServer/InstanceNameGoesHere"
+
+$database = "Covid19DataVault"
+$queryString = "EXEC Stage.upCovidTrackingDailyState "
+
+#write results to files
+$jsonFilePath = 'T:\Documents\kDSResearch2020\CovidTrackingDailyState.json'
+Set-Content $jsonFilePath  $fromWeb  #ouputs file to json file
+
+Write-Host "Executing SQL..."
+
+CLS
+
+#process JSON --execute store procedure to parse JSON file in SQL Server
+Invoke-Sqlcmd -ServerInstance $destinationServerName -Database $database -Query $queryString
+
+Write-Host "Done..."
+
+
+ 
