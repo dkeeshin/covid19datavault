174dad8f1a06e655bf751e5b326753462df04359
dkeeshin <62847782+dkeeshin@users.noreply.github.com>
2020-06-06 17:11:55 -0500

    OWID Sample JSON Read from Powershell

---
+++ 095SampleOWIDJsonFormat.ps1
@@ -0,0 +1,19 @@
+﻿##sample JSON reading of recommended OWID Covid19 Country file JSON format
+##dkeeshin@keeshinds,com
+
+$request = 'https://raw.githubusercontent.com/dkeeshin/covid19datavault/master/OWIDSampleCountry.json'
+$jsonFromTheWeb = Invoke-WebRequest $request
+
+CLS
+
+$j = $jsonFromTheWeb|ConvertFrom-Json
+$dateToFind = "2020-06-03"
+
+
+ foreach ($f in $j ) { 
+      if ($f.date -eq $dateToFind ) {$f|ConvertTo-Json } 
+            }
+
+
+
+
