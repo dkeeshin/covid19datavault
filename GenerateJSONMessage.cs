3d309fd832a032ed5fcb8d4080420cc93096b789
dkeeshin <62847782+dkeeshin@users.noreply.github.com>
2020-06-09 10:40:06 -0500

    update 2020-06-08
    includes session ID

--- GenerateJSONMessage.cs
+++ GenerateJSONMessage.cs
@@ -5,46 +5,47 @@ using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
-using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace GenerateJSONMessage
 {
-			
+
 	class SendToQueue
 	{
 
 		static IQueueClient queueClient;
 
 		static void Main(string[] args)
-   		{
-			String currentID;
-			String fileNumber;
-			String connectionString;
+		{
+
+			String connectionStringLocal;
 			String storedProcedure01 = "";
 			String storedProcedure02 = "";
-			string serviceBusConnection;
+			String serviceBusConnection;
 			String queueName = "";
+			String sessionID;
 			String jsonString;
+			
 
-			switch (args[0])
+			switch (args[0].ToLower())
 			{
-				case "ProvinceState":
+				case "provincestate":
 					Console.WriteLine("ProvinceState");
 					storedProcedure01 = "Message.upGetProvinceStateDetailItem";
 					storedProcedure02 = "Message.upFlagProvinceStateDetailItem";
 					queueName = "datavaultqueue01";
 					break;
-				case "Country":
+				case "country":
 					Console.WriteLine("Country");
 					storedProcedure01 = "Message.upGetCountryDetailItem";
 					storedProcedure02 = "Message.upFlagCountryDetailItem";
 					queueName = "datavaultqueue02";
 					break;
-				case "County":
+				case "county":
 					Console.WriteLine("County");
 					storedProcedure01 = "Message.upGetCounTYDetailItem";
 					storedProcedure02 = "Message.upFlagCounTYDetailItem";
-					queueName = "datavaultqueue03";
+					queueName = "datavaultqueue03";  //remember to change prior to deployment sb 03
 					break;
 				default:
 					Console.WriteLine("Default");
@@ -55,39 +56,54 @@ namespace GenerateJSONMessage
 					break;
 			}
 
-			connectionString = ConfigurationManager.AppSettings.Get("DatabaseServer");
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			connectionStringLocal = ConfigurationManager.AppSettings.Get("DatabaseServer");
 			serviceBusConnection = ConfigurationManager.AppSettings.Get("ServiceBusConnection");
-			// Read a particular key from the config file            
-			Console.WriteLine("The value of DatabaseServer is: " + connectionString);
-			Console.WriteLine("The value of ServiceBusConnector is: " + serviceBusConnection);
+
+			SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+			builder.DataSource = "azuresqldatabaseserver.database.windows.net";
+			builder.UserID = "azureUser";
+			builder.Password = "passwordGoesHere";
+			builder.InitialCatalog = "Covid19DataVaultForAzure";
+
+			Console.WriteLine("The value of Remote DatabaseServer is: " + builder.DataSource);
 
 
 			// create and open a connection object
-			SqlConnection conn01 = new SqlConnection(connectionString);
-			conn01.Open();
+			SqlConnection connection01 = new SqlConnection(connectionStringLocal);
+			connection01.Open();
 
-			//create a second connection for flagging file create item as done
-			SqlConnection conn02 = new SqlConnection(connectionString);
-			conn02.Open();
-			
+			//create a second connection for flagging sent message 
+			SqlConnection connection02 = new SqlConnection(connectionStringLocal);
+			connection02.Open();
+
+
 			// create a command object identifying the stored procedure
-			SqlCommand cmd = new SqlCommand(storedProcedure01, conn01)
+			SqlCommand command01 = new SqlCommand(storedProcedure01, connection01)
 			{   // set the command object so it knows to execute a stored procedure
 				CommandType = CommandType.StoredProcedure
 			};
 
 			// create a second command object identifying the stored procedure
-			SqlCommand cmd02 = new SqlCommand(storedProcedure02, conn02)
+			SqlCommand command02 = new SqlCommand(storedProcedure02, connection02)
 			{   // set a second  command object so it knows to execute a stored procedure
 				CommandType = CommandType.StoredProcedure
 			};
 
+
 			//Input parameter for second stored procedure - flags item by ID
-			SqlParameter IDinput = cmd02.Parameters.Add("@ID", SqlDbType.Int);
-			IDinput.Direction = ParameterDirection.Input;
-			SqlDataReader reader = cmd.ExecuteReader();
+			SqlParameter HashValue01 = command02.Parameters.Add("@hashValue", SqlDbType.VarBinary);
+			HashValue01.Direction = ParameterDirection.Input;
+
+
+			SqlDataReader reader = command01.ExecuteReader();
+
 			Console.WriteLine("\nGenerating JSON Message...\n");
-				
+			int rowNumber = 1;
+
+			sessionID = Guid.NewGuid().ToString();
 
 			try
 			{
@@ -95,46 +111,65 @@ namespace GenerateJSONMessage
 				//get current row from SQL table - keep executing reads after items flagged as complete
 				while (reader.Read())
 				{
-					fileNumber = reader.GetValue(2).ToString();  //assign filenumber from SQL Server row 
-					
 					jsonString = reader.GetValue(0).ToString();
-					//method to send Service Bus message
-					SendIt(serviceBusConnection, queueName, jsonString).GetAwaiter().GetResult();
+					Console.WriteLine(jsonString);
 
-					currentID = reader.GetValue(1).ToString(); //get value as a string
-					IDinput.Value = Convert.ToInt32(currentID);  //convert it to int
-					cmd02.ExecuteNonQuery(); //execute stored proc2 to flag file status as created
-					reader.Close();  //close current reader
-					reader = cmd.ExecuteReader();  //get next item
+					//log sent item by hash to Message.SentCounTYDetail table
+					HashValue01.Value = reader.GetSqlBinary(1);
+
+					
+					//method to send Service Bus message
+					SendIt( sessionID, serviceBusConnection, queueName, jsonString).GetAwaiter().GetResult();
+
+					//update Sent table
+					command02.ExecuteNonQuery();
+
+					//delay to allow Azure SQL Databse to enough time to Message.Confirm hash from target 
+					//int waitPeriod = 233;
+					//Console.WriteLine($"Wait period in (ms): {waitPeriod.ToString()}");
+					//System.Threading.Thread.Sleep(waitPeriod);
+
+
+					rowNumber += 1;
+
 				}
 			}
 			catch (Exception exception)
 			{
 				Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
-				if (conn01!= null)
-				{conn01.Close(); }
+				if (connection01 != null)
+				{ connection01.Close(); }
 				if (reader != null)
-				{reader.Close();}
-				if (conn02 != null)
-				{conn02.Close();}
+				{ reader.Close(); }
+				if (connection02 != null)
+				{ connection02.Close(); }
+
 			}
 			finally
 			{
-				conn01.Close();
-				conn02.Close();
+				connection01.Close();
+				connection02.Close();
 			}
-				Console.WriteLine("\nDone\n");
+
+			Console.WriteLine("\nDone with transmit...\n");
+			stopwatch.Stop();
+			TimeSpan stopwatchElapsed = stopwatch.Elapsed;
+			Console.WriteLine("Total Seconds: " + Convert.ToInt32(stopwatchElapsed.TotalSeconds));
+
 		}
 
-		static async Task SendIt(string serviceBusConnection, string queueName, string jsonString)
+		static async Task SendIt(string sessionID, string serviceBusConnection, string queueName, string jsonString)
 		{
 			try
 			{
-			   queueClient = new QueueClient(serviceBusConnection, queueName);
+				queueClient = new QueueClient(serviceBusConnection, queueName);
 
 				var messageBody = jsonString;
-				var message = new Message(Encoding.UTF8.GetBytes(messageBody));
 				
+				var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+				message.SessionId = sessionID;
+
 				// Send the message to the queue
 				await queueClient.SendAsync(message);
 
@@ -143,13 +178,14 @@ namespace GenerateJSONMessage
 			{
 				Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
 			}
-			
+
 		}
+
 
 	}
 }
-	
-		
 
-	
+
+
+
 
